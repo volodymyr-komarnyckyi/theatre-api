8d15aabebd71fff030f1c51f7999eaaed4116630
user <volodimirkomarnickij28@gmail.com>
2023-08-09 22:17:39 +0300

    Add Ticket and Reservation models

--- theatre/models.py
+++ theatre/models.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 
 class Genre(models.Model):
@@ -50,3 +52,53 @@ class Performance(models.Model):
 
     def __str__(self):
         return f"{self.play.title} in theatre hall: {self.theatre_hall.id}"
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+class Ticket(models.Model):
+    performance = models.ForeignKey(
+        Performance, on_delete=models.CASCADE, related_name="tickets"
+    )
+    reservation = models.ForeignKey(
+        Reservation, on_delete=models.CASCADE, related_name="tickets"
+    )
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    def clean(self):
+        for ticket_attr_value, ticket_attr_name, theatre_hall_attr_name in [
+            (self.row, "row", "count_rows"),
+            (self.seat, "seat", "count_seats_in_row"),
+        ]:
+            count_attrs = getattr(
+                self.performance.theatre_hall, theatre_hall_attr_name
+            )
+            if not (1 <= ticket_attr_value <= count_attrs):
+                raise ValidationError(
+                    {
+                        ticket_attr_name: f"{ticket_attr_name} number "
+                        f"must be in available range: "
+                        f"(1, {theatre_hall_attr_name}): "
+                        f"(1, {count_attrs})"
+                    }
+                )
+
+    def __str__(self):
+        return (
+            f"{str(self.performance)} (row: {self.row}, seat: {self.seat})"
+        )
+
+    class Meta:
+        unique_together = ("performance", "row", "seat")
