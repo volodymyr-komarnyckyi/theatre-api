5af5b0c87bab8049cfa5082b9daff3be890bc7d5
user <volodimirkomarnickij28@gmail.com>
2023-08-09 23:03:15 +0300

    Create PlayListSerializer and PlayDetailSerializer. Improved PlayViewSet for different actions

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -11,12 +11,44 @@ class GenreSerializer(serializers.ModelSerializer):
 
 
 class ActorSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Actor
         fields = "__all__"
 
 
 class PlaySerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Play
         fields = ("id", "title", "description", "genres", "actors")
+
+
+class PlayListSerializer(serializers.ModelSerializer):
+    genres = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="name"
+    )
+    actors = serializers.SlugRelatedField(
+        many=True,
+        read_only=True,
+        slug_field="full_name",
+    )
+
+    class Meta:
+        model = Play
+        fields = ("id", "title", "description", "genres", "actors")
+
+
+class PlayDetailSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+    actors = ActorSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Play
+        fields = ("id", "title", "description", "genres", "actors")
+
+
+
+
+
+
--- theatre/views.py
+++ theatre/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 
 from theatre.models import Genre, Actor, Play
-from theatre.serializers import GenreSerializer, ActorSerializer, PlaySerializer
+from theatre.serializers import GenreSerializer, ActorSerializer, PlaySerializer, PlayListSerializer
 
 
 class GenreViewSet(viewsets.ModelViewSet):
@@ -18,3 +17,22 @@ class ActorViewSet(viewsets.ModelViewSet):
 class PlayViewSet(viewsets.ModelViewSet):
     queryset = Play.objects.all()
     serializer_class = PlaySerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+
+        if self.action in ("list", "retrieve"):
+            queryset = queryset.prefetch_related("genres", "actors")
+
+        return queryset
+
+    def get_serializer_class(self):
+        serializer_class = self.serializer_class
+
+        if self.action == "list":
+            serializer_class = PlayListSerializer
+
+        elif self.action == "retrieve":
+            serializer_class = PlayListSerializer
+
+        return serializer_class
