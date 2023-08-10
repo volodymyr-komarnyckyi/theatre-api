24a95e7187404ad816c81691056d44ad2eb4300c
user <volodimirkomarnickij28@gmail.com>
2023-08-10 18:22:03 +0300

    Add path for login and view for Token Create

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, CreateTokenView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", CreateTokenView.as_view(), name="login"),
 ]
 
 app_name = "user"
