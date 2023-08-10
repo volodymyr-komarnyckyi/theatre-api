e193bbd085c087c23b2166dd965c2d9fbf8e3c95
user <volodimirkomarnickij28@gmail.com>
2023-08-10 16:28:09 +0300

    Add create method to ReservationSerializer

--- theatre/serializers.py
+++ theatre/serializers.py
@@ -89,13 +89,21 @@ class PerformanceDetailSerializer(PerformanceSerializer):
 class TicketSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Reservation
+        model = Ticket
         fields = ("id", "row", "seat", "performance")
 
 
 class ReservationSerializer(serializers.ModelSerializer):
-    tickets = TicketSerializer
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
 
     class Meta:
         model = Reservation
         fields = ("id", "tickets", "created_at")
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            reservation = Reservation.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(reservation=reservation, **ticket_data)
+            return reservation
