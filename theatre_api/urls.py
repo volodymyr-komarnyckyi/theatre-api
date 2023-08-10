6105f38f48b92822cf999a6aee270ea7d6414882
user <volodimirkomarnickij28@gmail.com>
2023-08-10 18:01:00 +0300

    Add serializer and view for Create User. Add path to register endpoint

--- theatre_api/urls.py
+++ theatre_api/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/theatre/", include("theatre.urls", namespace="theatre")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
