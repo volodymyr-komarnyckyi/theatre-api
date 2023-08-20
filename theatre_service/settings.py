b37d7e9a70c9ef9f03a2af9658b539e127d92c8e
user <volodimirkomarnickij28@gmail.com>
2023-08-20 23:06:19 +0300

    Fill README.md with up-to-date info

--- theatre_service/settings.py
+++ theatre_service/settings.py
@@ -25,8 +25,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     "rest_framework",
     "drf_spectacular",
+
     "theatre",
     "user",
 ]
